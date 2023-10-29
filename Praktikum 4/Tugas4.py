1513bbf24bd32b1884314427d6ebecf7d272fa2e
NizarKurnia <nizkur123@gmail.com>
2023-10-29 19:27:52 +0700

    DDP-2023

--- Praktikum 4/Tugas4.py
+++ Praktikum 4/Tugas4.py
@@ -4,12 +4,3 @@ Kendaraan.append('manual')
 print(Kendaraan)
 Kendaraan.insert(1,'supra')
 print(Kendaraan)
-
-Luasbangundatar = int(input('Apakah luas bangun datar ini?'))
-    match Luasbangundatar:
-        case 'luas persegi':
-            print()
-        case 'luas lingkaran':
-            print()
-        case 'luas segitiga':
-            print()
